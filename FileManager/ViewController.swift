4f19bec3b78dce450f6a88bf2832374f58d1b694
Fanil_Jr <ethic91@icloud.com>
2022-10-19 10:37:40 +0500

    Initial Commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Fanil_Jr on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
