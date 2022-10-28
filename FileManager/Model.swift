82a7feb917a31a0ef23ce4ddcf7a4b8da9be0d26
Fanil_Jr <ethic91@icloud.com>
2022-10-28 18:28:38 +0500

    ready FileManager

---
+++ FileManager/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  FileManager
+//
+//  Created by Fanil_Jr on 24.10.2022.
+//
+
+import Foundation
---
+++ FileManager/SettingsViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewCell.swift
+//  FileManager
+//
+//  Created by Fanil_Jr on 24.10.2022.
+//
+
+import Foundation
